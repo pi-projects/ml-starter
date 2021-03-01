be67f3a4474a8ee48862332f0983afb5b1c2a7f0
Bhavani, Praveen <pkbhavani@gmail.com>
2021-02-28 21:04:23 -0600

    added more algorithms

--- src/ml_starter/sentiment_analysis/algorithms.py
+++ src/ml_starter/sentiment_analysis/algorithms.py
@@ -19,6 +19,7 @@ def get_order(n_samples):
 def hinge_loss_single(feature_vector, label, theta, theta_0):
     """
     Finds the hinge loss on a single data point given specific classification parameters
+
     Args:
     :param feature_vector: A numpy array describing the given data point.
     :param label: A real valued number, the correct classification of the data point
@@ -55,7 +56,7 @@ def perceptron_single_step_update(feature_vector, label, current_theta, current_
     Property updates the classification parameter, theta and theta_0 on a single step of the perceptron
     algorithm.
 
-    Argss:
+    Args:
     :param feature_vector: A numpy array describing a single data point.
     :param label: The correct classification of the feature vector
     :param current_theta: The current theta being used by the perceptron algorithm before this update
@@ -81,14 +82,14 @@ def perceptron(feature_matrix, labels, T):
     NOTE: Iterate the data matrix by the orders returned by get_order(feature_matrix.shape[0])
 
     Args:
-        feature_matrix -  A numpy matrix describing the given data. Each row
+    :param feature_matrix: -  A numpy matrix describing the given data. Each row
             represents a single data point.
-        labels - A numpy array where the kth element of the array is the
+    :param labels: - A numpy array where the kth element of the array is the
             correct classification of the kth row of the feature matrix.
-        T - An integer indicating how many times the perceptron algorithm
+    :param T: - An integer indicating how many times the perceptron algorithm
             should iterate through the feature matrix.
 
-    Returns: A tuple where the first element is a numpy array with the value of
+    :return:  A tuple where the first element is a numpy array with the value of
     theta, the linear classification parameter, after T iterations through the
     feature matrix and the second element is a real number with the value of
     theta_0, the offset classification parameter, after T iterations through
@@ -114,16 +115,15 @@ def average_perceptron(feature_matrix, labels, T):
 
     NOTE: Iterate the data matrix by the orders returned by get_order(feature_matrix.shape[0])
 
-
     Args:
-        feature_matrix -  A numpy matrix describing the given data. Each row
+    :param feature_matrix: -  A numpy matrix describing the given data. Each row
             represents a single data point.
-        labels - A numpy array where the kth element of the array is the
+    :param labels: - A numpy array where the kth element of the array is the
             correct classification of the kth row of the feature matrix.
-        T - An integer indicating how many times the perceptron algorithm
+    :param T: - An integer indicating how many times the perceptron algorithm
             should iterate through the feature matrix.
 
-    Returns: A tuple where the first element is a numpy array with the value of
+    :return: A tuple where the first element is a numpy array with the value of
     the average theta, the linear classification parameter, found after T
     iterations through the feature matrix and the second element is a real
     number with the value of the average theta_0, the offset classification
@@ -151,16 +151,16 @@ def pegasos_single_step_update(feature_vector, label, l, eta, current_theta, cur
     single step of the Pegasos algorithm
 
     Args:
-        feature_vector - A numpy array describing a single data point.
-        label - The correct classification of the feature vector.
-        L - The lamba value being used to update the parameters.
-        eta - Learning rate to update parameters.
-        current_theta - The current theta being used by the Pegasos
+    :param feature_vector: - A numpy array describing a single data point.
+    :param label:  - The correct classification of the feature vector.
+    :param l: - The lamba value being used to update the parameters.
+    :param eta:  - Learning rate to update parameters.
+    :param current_theta: - The current theta being used by the Pegasos
             algorithm before this update.
-        current_theta_0 - The current theta_0 being used by the
+    :param current_theta_0: - The current theta_0 being used by the
             Pegasos algorithm before this update.
 
-    Returns: A tuple where the first element is a numpy array with the value of
+    :return: A tuple where the first element is a numpy array with the value of
     theta after the current update has completed and the second element is a
     real valued number with the value of theta_0 after the current updated has
     completed.
@@ -181,18 +181,15 @@ def pegasos(feature_matrix, labels, T, L):
     where t is a counter for the number of updates performed so far (between 1
     and nT inclusive).
 
-
     Args:
-        feature_matrix - A numpy matrix describing the given data. Each row
-            represents a single data point.
-        labels - A numpy array where the kth element of the array is the
-            correct classification of the kth row of the feature matrix.
-        T - An integer indicating how many times the algorithm
+    :param feature_matrix: A numpy matrix describing the given data.
+            Each row represents a single data point.
+    :param labels: A numpy array where the kth element of the array is the correct classification of the kth row of the feature matrix.
+    :param T:An integer indicating how many times the algorithm
             should iterate through the feature matrix.
-        L - The lamba value being used to update the Pegasos
+    :param L:The lamba value being used to update the Pegasos
             algorithm parameters.
-
-    Returns: A tuple where the first element is a numpy array with the value of
+    :return: A tuple where the first element is a numpy array with the value of
     the theta, the linear classification parameter, found after T
     iterations through the feature matrix and the second element is a real
     number with the value of the theta_0, the offset classification
@@ -216,13 +213,12 @@ def classify(feature_matrix, theta, theta_0):
     data points.
 
     Args:
-        feature_matrix - A numpy matrix describing the given data. Each row
+    :param feature_matrix:- A numpy matrix describing the given data. Each row
             represents a single data point.
                 theta - A numpy array describing the linear classifier.
-        theta - A numpy array describing the linear classifier.
-        theta_0 - A real valued number representing the offset parameter.
-
-    Returns: A numpy array of 1s and -1s where the kth element of the array is
+    :param theta: - A numpy array describing the linear classifier.
+    :param theta_0: - A real valued number representing the offset parameter.
+    :return:  A numpy array of 1s and -1s where the kth element of the array is
     the predicted classification of the kth row of the feature matrix using the
     given theta and theta_0. If a prediction is GREATER THAN zero, it should
     be considered a positive classification.
